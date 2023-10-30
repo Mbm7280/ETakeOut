4add00998d8a8b556c22ac797231551ade092f91
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2023-10-30 21:05:08 +0800

    初始化

--- src/main/java/com/echo/service/DishFlavorService.java
+++
@@ -1,7 +0,0 @@
-package com.echo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.echo.entity.DishFlavor;
-
-public interface DishFlavorService extends IService<DishFlavor> {
-}
