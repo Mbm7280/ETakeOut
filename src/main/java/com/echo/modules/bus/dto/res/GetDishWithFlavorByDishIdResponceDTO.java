88e250ac7ed1706b618c6775527db0899212818c
Mbm7280 <56158949+Mbm7280@users.noreply.github.com>
2023-11-11 17:09:04 +0800

    develop dish interface

---
+++ src/main/java/com/echo/modules/bus/dto/res/GetDishWithFlavorByDishIdResponceDTO.java
@@ -0,0 +1,22 @@
+package com.echo.modules.bus.dto.res;
+
+import com.echo.modules.bus.model.BusDish;
+import com.echo.modules.bus.model.BusDishFlavor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetDishWithFlavorByDishIdResponceDTO extends BusDish {
+
+    //菜品对应的口味数据
+    private List<BusDishFlavor> flavors = new ArrayList<>();
+
+}
--- src/main/java/com/echo/modules/bus/service/BusDishService.java
+++ src/main/java/com/echo/modules/bus/service/BusDishService.java
@@ -1,5 +1,11 @@
 package com.echo.modules.bus.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.echo.config.api.Result;
+import com.echo.modules.bus.dto.req.AddDishRequestDTO;
+import com.echo.modules.bus.dto.req.UpdateDishByDishIdRequestDTO;
+import com.echo.modules.bus.dto.res.GetDishWithFlavorByDishIdResponceDTO;
+import com.echo.modules.bus.dto.res.GetPageDishListResponceDTO;
 import com.echo.modules.bus.model.BusDish;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -12,5 +18,44 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2023-11-01
  */
 public interface BusDishService extends IService<BusDish> {
+
+
+    /**
+     * 新增菜品
+     *
+     * @param dishDto
+     * @return
+     */
+    Result addDish(AddDishRequestDTO dishDto);
+
+
+    /**
+     * 菜品信息分页查询
+     *
+     * @param pageNum
+     * @param pageSize
+     * @param dishName
+     * @return
+     */
+    Result<Page<GetPageDishListResponceDTO>> getPageDishList(int pageNum, int pageSize, String dishName);
+
+
+    /**
+     * 根据id查询菜品信息和对应的口味信息
+     *
+     * @param dishId
+     * @return
+     */
+    Result<GetDishWithFlavorByDishIdResponceDTO> getDishWithFlavorByDishId(Long dishId);
+
+
+    /**
+     * 修改菜品
+     *
+     * @param updateDishByDishIdRequestDTO
+     * @return
+     */
+    Result updateDish(UpdateDishByDishIdRequestDTO updateDishByDishIdRequestDTO);
+
 
 }
